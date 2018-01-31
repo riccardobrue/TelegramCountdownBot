5b9668beb25312e9226f19a784aaa071d3e58406
Riccardo Bruè <riccardo.brue@gmail.com>
2018-01-31 06:11:38 +0000

    Update 4

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
-# THIS IS THE ENTRY POINT FOR OPENSHIFT
-from flask import Flask, jsonify
-from datetime import datetime
-import os
-import requests
-import random
+#!/usr/bin/env python
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route("/b")
-def press():
-    #r = requests.post(urlrow.url, json=details)
-    return jsonify({"ButtonPress": "Success"})
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-if __name__ == "__main__":
-    app.run()
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+
+def run():
+    print('starting server...')
+
+    # Server settings
+    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+    server_address = (' 172.30.254.23', 5000)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('running server...')
+    httpd.serve_forever()
+
+
+run()
