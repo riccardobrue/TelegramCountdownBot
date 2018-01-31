193837d471fd3d256511da06757d59e034e4c7a0
Riccardo Bruè <riccardo.brue@gmail.com>
2018-01-31 05:19:21 +0000

    Update 1

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 #
 # THIS IS THE ENTRY POINT FOR OPENSHIFT
 #
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
 
-from countdown_bot import app as application
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-#
-# Below for testing only
-#
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+from http import HTTPStatus
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Hello, Python!')
+        return
+
+
+def run(server_class=HTTPServer, handler_class=MyHandler):
+    server_address = ('localhost', 8000)
+    httpd = server_class(server_address, handler_class)
+    try:
+        print("Server works on http://localhost:8000")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Stop the server on http://localhost:8000")
+        httpd.socket.close()
+
+
+if __name__ == '__main__':
+    run()
