c33b226e78f7394ad4b0178fd63ef7a02066090d
Riccardo Bruè <riccardo.brue@gmail.com>
2018-01-31 15:53:15 +0000

    Huge updates

--- myapp/views.py
+++ myapp/views.py
@@ -8,12 +8,12 @@ def index(request):
     string = "Hello, I am a bot response." + str(request);
 
     # imposto l'URL per inviare i messaggi indietro al bot
-    URL = 'https://api.telegram.org/bot' + "543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc" + '/sendMessage'
+    URL = 'https://api.telegram.org/bot543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc/sendMessage'
 
     # invio indietro alla chat ID il messaggio ricevuto
     richiesta = requests.get(URL, verify=False, data={'chat_id': request, 'text': string})
 
-    return HttpResponse(richiesta)
+    return HttpResponse(string)
 
 
 def bot_response(request, bot_response_id):
@@ -21,12 +21,12 @@ def bot_response(request, bot_response_id):
     string = "Hello, I am a bot response."+response_id;
 
     # imposto l'URL per inviare i messaggi indietro al bot
-    URL='https://api.telegram.org/bot' + "543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc" + '/sendMessage'
+    URL='https://api.telegram.org/bot543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc/sendMessage'
 
     # invio indietro alla chat ID il messaggio ricevuto
     richiesta = requests.get(URL, verify=False, data={'chat_id': bot_response_id, 'text': string})
     logger.info("Loading handlers for telegram bot"+richiesta)
-    return HttpResponse(richiesta)
+    return HttpResponse(string)
 
 
 
