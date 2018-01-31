c4d43d008301d5c2dae1850bcac3c6afcbd3010c
Riccardo Bruè <riccardo.brue@gmail.com>
2018-01-31 12:43:48 +0000

    Django added and updated

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=5xqooje3k6*ozbqytmh(7e-znt_13f3!!a@1d3#9h$f8f36*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
