dfaa404406307b43bc9092eefa760e875070f439
Riccardo Bruè <riccardo.brue@gmail.com>
2018-01-31 05:42:27 +0000

    Update 2

--- app.py
+++ app.py
@@ -5,18 +5,7 @@ import os
 import requests
 import random
 
-user = os.environ["MYSQL_USER"]
-passwd = os.environ["MYSQL_PASSWORD"]
-dbhost = os.environ["MYSQL_SERVICE_HOST"]
-dbname = os.environ["MYSQL_DATABASE"]
-
 application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = \
-    'mysql://%s:%s@%s/%s' % (user, passwd, dbhost, dbname)
-
-application.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 @application.route("/buttonpress")
 def press():
