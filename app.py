d68b9377997b0278e67bfe3015fc3340c681d644
Riccardo Bruè <riccardo.brue@gmail.com>
2018-02-01 03:27:19 +0000

    Update 1

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import logging
 def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
+
 def start(bot, update):
-     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!"+update.message.chat_id)
 
 
 updater = Updater('543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc')
