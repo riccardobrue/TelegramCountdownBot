587e169ca3ad237453e3ed0fa655b1fc00e4593f
Riccardo Bruè <riccardo.brue@gmail.com>
2018-02-01 02:21:52 +0000

    Massive update 1

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import path
+from django.conf.urls import url, include
+
 
 urlpatterns = [
-    path('', include('myapp.urls')),
-    path('myapp/', include('myapp.urls')),
     path('admin/', admin.site.urls),
-
+	#url(r'^admin/', admin.site.urls),
+    url(r'^', include('telegrambot.urls')),
+    url(r'^$', include('bot.urls')),
 ]
