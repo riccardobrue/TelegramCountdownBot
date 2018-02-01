8dae7ae157c5306028f24a3e88987997b1d21014
Riccardo Bruè <riccardo.brue@gmail.com>
2018-02-01 02:48:07 +0000

    Massive update 1

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from telegram.ext import Updater, CommandHandler
-
+import logging
 
 def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
+
+def start(bot, update):
+     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
 updater = Updater('543129108:AAE13LpVqITEfI-DmzGkQP1rRPvq6fzuLQc')
+dispatcher = updater.dispatcher
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('hello', hello))
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
 
 updater.start_polling()
-updater.idle()
+#updater.idle()
