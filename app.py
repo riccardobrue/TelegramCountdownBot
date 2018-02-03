dce43e84da3f5937a62f4ee3efb597c409022999
Riccardo Bruè <riccardo.brue@gmail.com>
2018-02-03 14:34:11 +0000

    Update 2

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from telegram.ext import Updater, CommandHandler
 import logging
 import db_manager
-import datetime
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+DATE, MESSAGE  = range(2)
 
 
 def hello(bot, update):
@@ -61,10 +65,78 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
+#==============================================================================================
+def clear(user_data):
+    if 'data' in user_data:
+        del user_data['data']
+    user_data.clear()
+#==========================------------------------------------
+def timer_start(bot, update):
+    user = update.message.from_user
+    logger.info("Start received from %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Hello! Set up your countdown, please send me a date (dd/mm/yyyy)')
+    return DATE
+#==========================------------------------------------
+def set_timer_date(bot, update,user_data):
+    user = update.message.from_user
+    logger.info("Countdown date from %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Perfect! If you want you can set a message for this countdown!')
+    user_data['data'] = update.message.text
+    return MESSAGE
+#==========================------------------------------------
+def set_timer_message(bot, update,user_data):
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    logger.info("Message from %s: %s", user.first_name, update.message.text)
+
+    db_manager.add(chat_id,user.first_name,update.message.text,user_data['data'],0)
+    savedCountdown=db_manager.getSingle(chat_id,user.first_name,0)
+    update.message.reply_text('Message acquired and countdown set. Bye! ('+savedCountdown+")")
+
+    clear(user_data)
+    return ConversationHandler.END
+#==========================------------------------------------
+def skip_timer_message(bot, update,user_data):
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    logger.info("User %s did not send any message.", user.first_name)
+
+    insertionMessage=db_manager.add(chat_id, user.first_name,None, user_data['data'], 0)
+    savedCountdown = db_manager.getSingle(chat_id, user.first_name, 0)
+    update.message.reply_text('Countdown set. Bye! (' + savedCountdown +"_"+insertionMessage +")")
+
+    clear(user_data)
+    return ConversationHandler.END
+#==========================------------------------------------
+def dismiss(bot, update,user_data):
+    user = update.message.from_user
+    logger.info("User %s dismissed the conversation.", user.first_name)
+    update.message.reply_text('Dismissed. Bye!')
+
+    clear(user_data)
+    return ConversationHandler.END
+
+#==============================================================================================
+
+
 def openshiftStart():
     updater = Updater('541177999:AAE3-K_4-pj7WMMLnjS4PPnG1NeHdMiqVa4')
     dispatcher = updater.dispatcher
-    #queue = updater.job_queue
+
+    # ==============================================================================================
+    # Add conversation handler with the states DATE and MESSAGE
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', timer_start)],
+        states={
+            DATE: [RegexHandler('^([0]?[1-9]|[1|2][0-9]|[3][0|1])[/]([0]?[1-9]|[1][0-2])[/]([0-9]{4}|[0-9]{2})$', set_timer_date,pass_user_data=True)],
+
+            MESSAGE: [MessageHandler(Filters.text, set_timer_message,pass_user_data=True),
+                    CommandHandler('skip_message', skip_timer_message)],
+        },
+        fallbacks=[CommandHandler('dismiss', dismiss)]
+    )
+    dispatcher.add_handler(conv_handler)
+    # ==============================================================================================
 
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('hello', hello))
@@ -73,6 +145,7 @@ def openshiftStart():
                                   pass_args=True,
                                   pass_job_queue=True,
                                   pass_chat_data=True))
+
     dispatcher.add_handler(CommandHandler("unset", unset, pass_chat_data=True))
 
     # log all errors
@@ -84,13 +157,25 @@ def openshiftStart():
     updater.idle()
 
 
+
+
+
+
+
+
 def localTesting():
     message=db_manager.add(30,"test123","testing message","05/02/2018",0)
     print(message)
-    message=db_manager.get(31,"test123",0)
+    message=db_manager.edit(30,"test123","test345","06/03/2019",0)
+    print(message)
+    message=db_manager.getSingle(30,"test123",0)
     print(message)
 
+    record=db_manager.getAll(30,"test123")
+    for doc in record:
+        print(doc)
 
 
-#openshiftStart()
-localTesting()
+
+openshiftStart()
+#localTesting()
--- db_manager.py
+++ db_manager.py
@@ -1,38 +1,41 @@
 from pymongo import MongoClient
 import datetime
-import pprint
-
-
 
 def initdb():
-    #client = MongoClient('mongodb://mongodb:mongodbpassword@mongodb/db') #from openshift
-    client = MongoClient('mongodb://localhost:27017/db') #local
+    client = MongoClient('mongodb://mongodb:mongodbpassword@mongodb/db')    #from OpenShift
+    #client = MongoClient('mongodb://localhost:27017/db')                   #local
 
     db = client.db
     try:
         db.create_collection("countdowns")  # create a new collection called "countdowns"
     except:
         pass
-
     return db.countdowns #get the collection
 
-def add(chatId, chatName, message, date, counter):
-    collections=initdb()
-    record = collections.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
 
+def add(chatId, chatName, message, date, counter):
+    collection=initdb()
+    record = collection.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
     if (record == None):
         targetDate = datetime.datetime.strptime(date, '%d/%m/%Y')
         today = datetime.datetime.utcnow()
 
         if(today<targetDate):
             # Add a document in the collection
-            countdown = {"chartid": chatId,
-                         "chatName": chatName,
-                         "message":message,
-                         "date": targetDate,
-                         "counter": counter}
+            if(message==None):
+                countdown = {"chartId": chatId,
+                             "chatName": chatName,
+                             "date": targetDate,
+                             "counter": counter}
+            else:
+                countdown = {"chartId": chatId,
+                             "chatName": chatName,
+                             "message":message,
+                             "date": targetDate,
+                             "counter": counter}
 
-            insertedId=collections.insert_one(countdown).inserted_id
+            insertedId=collection.insert_one(countdown).inserted_id
+            #SET THE NEXT COUNTDOWN
             return "Date saved for the countdown!"
         else:
             return "Cannot countdown to the past!"
@@ -43,22 +46,40 @@ def add(chatId, chatName, message, date, counter):
 
 
 def edit(chatId, chatName, newmessage, newdate, counter):
-    collections = initdb()
-    record = collections.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
-
+    collection = initdb()
+    record = collection.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
     if (record == None):
         return "Cannot find the countdown!"
     else:
-        return "TO DO!"
+        targetDate = datetime.datetime.strptime(newdate, '%d/%m/%Y')
+        today = datetime.datetime.utcnow()
+
+        if(today<targetDate):
+            countdown = {"chartid": chatId,
+                         "chatName": chatName,
+                         "message": newmessage,
+                         "date": targetDate,
+                         "counter": counter}
+
+            collection.update_one({'_id': record["_id"] }, {"$set": countdown})
+            # SET THE NEXT COUNTDOWN
+            return "Record updated!"
+        else:
+            return "Cannot countdown to the past!"
 
 
-
-def get(chatId,chatName,counter):
-    collections=initdb()
-    record=collections.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
+def getSingle(chatId,chatName,counter):
+    collection=initdb()
+    record=collection.find_one({'chartid': chatId, 'chatName': chatName, 'counter': counter})
     if(record==None):
         return "None object found"
     else:
-        pprint.pprint(record)
+        return str(record)
 
-    return str(record)
+def getAll(chatId,chatName):
+    collection=initdb()
+    record=collection.find({'chartid': chatId, 'chatName': chatName})
+    if(record==None):
+        return "None object found"
+    else:
+        return record
